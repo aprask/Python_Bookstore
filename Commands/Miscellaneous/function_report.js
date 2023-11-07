014ea3172482b0750334556fd9b138875d276732
aprask <a.praskala@tutanota.com>
2023-11-06 19:49:36 -0500

    Initial commit

---
+++ Commands/Miscellaneous/function_report.js
@@ -0,0 +1,6 @@
+document.getElementById("closeButton").addEventListener("click", function()
+{
+    window.close();
+});
+// simple function that closes the html window (activated by a pointer click)
+// the reference id = "closeButton"
